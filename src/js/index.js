fb2b08f3aaa00f19c8a75df35946b9fb9fbcb7d3
Rocio <144937216+Rociosantos18@users.noreply.github.com>
2024-01-10 20:36:05 +0000

    last commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,30 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+// import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let counter = 0;
+
+const SimpleCounter = () => {
+    return (
+    <div className="counter">  
+        <div><i className="fa-regular fa-clock"></i></div>
+        <div className="four">{Math.floor(counter/1000) % 10}</div>
+        <div className="three">{Math.floor(counter/100) % 10}</div>
+        <div className="tow">{Math.floor(counter/10) % 10}</div>
+        <div className="one">{Math.floor(counter/1) % 10}</div>
+    </div>   
+   )
+
+};
+
+
+
+setInterval(() => {
+
+    counter++;
+
+    ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+}, 1000);
+
